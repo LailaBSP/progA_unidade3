34ef017f65b37f0d973c0c88799f37d44387e7dd
Carolina Aragão <devcarolinaaragao@outlook.com>
2026-07-08 13:36:33 -0300

    Update main.py

--- progA_unidade3/src/main.py
+++ progA_unidade3/src/main.py
@@ -1,4 +1,19 @@
-from visao.janela import iniciar
+from modelo.desenho import Desenho
+from visao.janela import JanelaPrincipal
+from controlador.controlador import ControladorDesenho
 
-#a main basicamente serve para inicalizar
-iniciar()
+# 1. Instancia o Modelo
+modelo = Desenho()
+
+# 2. Instancia a Visão passando o modelo
+visao = JanelaPrincipal(modelo)
+
+# 3. Instancia o Controlador integrando ambos
+controlador = ControladorDesenho(modelo, visao)
+
+# 4. Conecta o controlador à visão
+visao.associar_controlador(controlador)
+
+# 5. Inicia a aplicação
+if __name__ == '__main__':
+    visao.iniciar()
