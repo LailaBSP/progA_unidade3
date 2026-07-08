93d0ec67cf8bb03d9a3c7b929a2d4168f445f24e
Carolina Aragão <devcarolinaaragao@outlook.com>
2026-07-08 13:34:27 -0300

    Update controlador.py

--- progA_unidade3/src/controlador/controlador.py
+++ progA_unidade3/src/controlador/controlador.py
@@ -1,77 +1,44 @@
-from modelo.figuras import (Linha,Rabisco,Retangulo,Oval,Circulo,Poligono)
+from modelo.figuras import Linha, Rabisco, Retangulo, Oval, Circulo, Poligono
 
-#estado do desenho
-figuras = []
-figura_nova = None
+class ControladorDesenho:
+    def __init__(self, modelo_desenho, visao_janela):
+        self.modelo = modelo_desenho
+        self.visao = visao_janela
 
-#pq as funcoes usam canvas tipo_figura_var
-def organizacaoDaVariavel(canvas_recebido, tipo_recebido):
-    global canvas, tipo_figura_var
-    canvas = canvas_recebido
-    tipo_figura_var = tipo_recebido
+    def gerenciar_clique(self, event, tipo_figura):
+        classes_figuras = {
+            'Linha': Linha, 'Rabisco': Rabisco, 'Retângulo': Retangulo,
+            'Oval': Oval, 'Circulo': Circulo, 'Polígono': Poligono
+        }
 
-
-def gerenciar_clique(event):
-    global figura_nova
-
-    tipo = tipo_figura_var.get()
-
-    classes_figuras = {
-        'Linha': Linha,
-        'Rabisco': Rabisco,
-        'Retângulo': Retangulo,
-        'Oval': Oval,
-        'Circulo': Circulo,
-        'Polígono': Poligono
-    }
-
-    if tipo == 'Polígono':
-
-        if figura_nova is None:
-            figura_nova = Poligono(event.x, event.y)
+        if tipo_figura == 'Polígono':
+            if self.modelo.figura_nova is None:
+                self.modelo.figura_nova = Poligono(event.x, event.y)
+            else:
+                self.modelo.figura_nova.adicionar_ponto(event.x, event.y)
         else:
-            figura_nova.adicionar_ponto(event.x, event.y)
+            classe = classes_figuras.get(tipo_figura, Linha)
+            self.modelo.figura_nova = classe(event.x, event.y)
+        
+        self.visao.atualizar_tela()
 
-    else:
+    def atualizar_movimento(self, event):
+        if self.modelo.figura_nova:
+            self.modelo.figura_nova.atualizar(event.x, event.y)
+            self.visao.atualizar_tela()
 
-        classe = classes_figuras.get(tipo, Linha)
-        figura_nova = classe(event.x, event.y)
+    def soltar_clique(self, event, tipo_figura):
+        if tipo_figura == 'Polígono':
+            return
 
-    desenhar_tudo()
+        if self.modelo.figura_nova and not self.modelo.figura_nova.esta_incompleta():
+            self.modelo.adicionar_figura(self.modelo.figura_nova)
+        self.modelo.figura_nova = None
+        self.visao.atualizar_tela()
 
-def atualizar_figura_nova(event):
-   global figura_nova
-   if figura_nova:
-       figura_nova.atualizar(event.x, event.y)
-       desenhar_tudo()
-
-def incluir_figura_nova(event):
-   global figura_nova
-   tipo = tipo_figura_var.get()
-  
-   # Polígono não deve terminar no soltar do mouse, apenas no clique duplo
-   if tipo == 'Polígono':
-       return
-
-
-   if figura_nova and not figura_nova.esta_incompleta():
-       figuras.append(figura_nova)
-   figura_nova = None
-   desenhar_tudo()
-
-def finalizar_poligono(event):
-   global figura_nova
-   tipo = tipo_figura_var.get()
-  
-   if tipo == 'Polígono' and figura_nova:
-       if not figura_nova.esta_incompleta():
-           figuras.append(figura_nova)
-       figura_nova = None
-       desenhar_tudo()
-
-def desenhar_tudo():
-   canvas.delete("all")
-   for fig in figuras:
-       fig.desenhar(canvas, tracejado=False)
-   if figura_nova:
-       figura_nova.desenhar(canvas, tracejado=True)
+    def duplo_clique(self, event, tipo_figura):
+        if tipo_figura == 'Polígono' and self.modelo.figura_nova:
+            if not self.modelo.figura_nova.esta_incompleta():
+                self.modelo.adicionar_figura(self.modelo.figura_nova)
+            self.modelo.figura_nova = None
+            self.visao.atualizar_tela()
