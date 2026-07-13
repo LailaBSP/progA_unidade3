86c805b0b7082d25e3c7bca10074518b47247abf
lavssval <122504688+lavssval@users.noreply.github.com>
2026-07-13 01:11:23 -0300

    docstring to main.py

--- progA_unidade3/src/main.py
+++ progA_unidade3/src/main.py
@@ -1,3 +1,12 @@
+"""Arquivo principal, é o que roda o programa. Cria o modelo, a visão
+e o controlador, liga tudo entre si e inicia a aplicação.
+
+@version 1.0
+@see modelo.desenho.Desenho
+@see visao.janela.JanelaPrincipal
+@see controlador.controlador.ControladorDesenho
+@since 1.0
+"""
 from modelo.desenho import Desenho
 from visao.janela import JanelaPrincipal
 from controlador.controlador import ControladorDesenho
